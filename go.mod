4e34596f705d5e68d2488798df1510eb96b283a8
amandeep <amandeep.chhabra2001@gmail.com>
2023-01-25 20:08:05 +0530

    My solution to the Go Module Assignment on Jan 25, 2023.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_module_assignment
+
+go 1.19
+
+require github.com/bxcodec/faker/v3 v3.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bxcodec/faker/v3"
+)
+
+type Person struct{
+	UserName string `faker:"username"`
+	FirstName string `faker:"first_name"`
+	LastName string `faker:"last_name"`
+	Email string `faker:"email"`
+	PhoneNumber string `faker:"phone_number"`
+}
+
+func main() {
+	person1 := Person{}
+	err := faker.FakeData(&person1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v", person1)
+}
